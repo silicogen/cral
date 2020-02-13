3b7ac82a06d807341c86e8d2c712afea909342cb
Алексей Чупров <alexey.chuprov@gmail.com>
2020-02-13 16:52:23 +0300

    Updates

---
+++ precache-manifest.9514f2a36b676f3461e77d4e122d369a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da68eab460f0a369308ea3afa5360711",
+    "url": "/cral/index.html"
+  },
+  {
+    "revision": "bbaf31c82b30b943a4cc",
+    "url": "/cral/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "89be93d3ef7d8e91e64b",
+    "url": "/cral/static/js/2.00264d53.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/cral/static/js/2.00264d53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bbaf31c82b30b943a4cc",
+    "url": "/cral/static/js/main.0c6aca4c.chunk.js"
+  },
+  {
+    "revision": "9857148294e364de56f9",
+    "url": "/cral/static/js/runtime-main.6a16cd4d.js"
+  }
+]);
